33cffbec14aa58e45364e7aa505babbd1ef2052d
mahdi jahed <mahdi.jax@gmail.com>
2019-02-14 18:33:02 +0330

    add group name

--- init.c
+++ init.c
@@ -21,6 +21,7 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1,"*******\n Amirhossein Mahmoodi\n Reza Poormeshki\n MohammadMahdi Jahed\n*******\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
