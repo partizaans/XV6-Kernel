66f21a3e5c9945fdfad411462d47453d3774db19
partizaans <r.pourmeshki@gmail.com>
2019-05-12 22:14:13 +0430

    Remove Extra shits

--- testthread.c
+++ testthread.c
@@ -23,27 +23,20 @@ void thread_3(void *arg_ptr) {
 }
 
 int main() {
-//    int pid_1;
-//    int pid_2;
-//    int pid_3;
     int arg = 101;
     void *arg_ptr = &arg;
     thread_create();
     thread_create();
     thread_create();
-    int tid;
-    thread_associate(&tid, thread_1, arg_ptr);
-    thread_associate(&tid, thread_2, arg_ptr);
-    thread_associate(&tid, thread_3, arg_ptr);
-//    pid_1 = thread_create(thread_1, arg_ptr);
-//    pid_2 = thread_create(thread_2, arg_ptr);
-//    pid_3 = thread_create(thread_3, arg_ptr);
-//    printf(1, "pid1: %d\t", pid_1);
-//    printf(1, "pid2: %d\t", pid_2);
-//    printf(1, "pid3: %d\n", pid_3);
+    int tid1, tid2,tid3;
+    thread_associate(&tid1, thread_1, arg_ptr);
+    thread_associate(&tid2, thread_2, arg_ptr);
+    thread_associate(&tid3, thread_3, arg_ptr);
+    printf(1, "tid1: %d\t", tid1);
+    printf(1, "tid2: %d\t", tid2);
+    printf(1, "tid3: %d\n", tid3);
     thread_join();
     thread_join();
     thread_join();
     exit();
-    return 0;
 }
